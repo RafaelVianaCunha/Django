d146f3aa96fdff314ab01229bae117a28dffe2a8
Rafael Viana <rafael.cunha@al.infnet.edu.br>
2015-12-15 19:59:26 -0200

    Projeto iniciado

---
+++ perfis/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from models import Perfil
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def exibir(request, perfil_id):
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfis.html', {"perfil": perfil})
