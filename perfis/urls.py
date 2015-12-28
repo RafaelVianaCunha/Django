5ba2c6b6bc9e0c5f58c807fac7c5cb1398317c84
Rafael Viana <rafael.cunha@al.infnet.edu.br>
2015-12-28 19:46:46 -0200

    adicionando função convidar

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from perfis import views
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
 
-                       url(r'^$', 'perfis.views.index', name='index'),
-                       url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name='exibir'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+                       url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
                        )
