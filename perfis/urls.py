6d49829df078192c5f43505bedba4cd6c81c7969
Rafael Viana <Rafael@Rafael-NoteBook.home>
2016-01-07 17:40:22 -0200

    Adicionado aceitar contato

--- perfis/urls.py
+++ perfis/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
                        url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
+                       url(r'^perfis/(?P<convite_id>\d+)/aceitar$', views.aceitar, name='aceitar'),
                        )
