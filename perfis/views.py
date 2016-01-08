1205d7b3f31ae2ac5d6a42b9691cee6cdd2f34ab
Rafael Viana <Rafael@Rafael-NoteBook.home>
2016-01-08 16:16:00 -0200

    Autenticando e autorizando usuários

--- perfis/views.py
+++ perfis/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from models import Perfil, Convite
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+@login_required
 def index(request):
      return render(request, 'index.html', { 'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
     ja_eh_contato = perfil in perfil_logado.contatos.all()
     return render(request, 'perfis.html', {"perfil": perfil, 'perfil_logado' : get_perfil_logado(request), "ja_eh_contato" : ja_eh_contato})
 
+@login_required
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
@@ -19,9 +22,11 @@ def convidar(request, perfil_id):
     return redirect('index')
 
 # Simulando usuario logado
+@login_required
 def get_perfil_logado(request):
-   return Perfil.objects.get(id=1)
+   return request.user.perfil
 
+@login_required
 def aceitar(request, convite_id):
     convite = Convite.objects.get(id=convite_id)
     convite.aceitar();
